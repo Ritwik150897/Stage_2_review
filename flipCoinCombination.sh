270f872ee795a1747251e5a590d853b1b0471648
Ritwik <deshrutu@gmail.com>
2022-04-22 12:44:05 +0530

    Coins Updated

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,16 +1,25 @@
-declare -A coin;
-coin[side1]=0;
-coin[side2]=0;
+heads=0;
+tails=0;
 
-for ((i=0; i<=10; i++))
+for((i=1; i<=10; i++))
 do
-	flip=$((RANDOM%2));
-	if [ $flip -eq 1 ]
-	then
-		echo H;
-		coin["side$flip"]++;
-	else
-		echo T;
-		coin["side$flip"]++;
-	fi
+   flip=$((RANDOM%2));
+   if [ $flip -eq 1 ]
+   then
+      echo H;
+      $((heads++));
+   else
+      echo T;
+      $((tails++));
+   fi
 done
+echo $heads
+echo $tails
+
+a=`echo $heads 10 | awk '{print $1/$2}'`;
+b=`echo $tails 10 | awk '{print $1/$2}'`;
+
+x=`echo $a 100 | awk '{print $1*$2}'`;
+echo "Heads: "$x "%"
+y=`echo $b 100 | awk '{print $1*$2}'`;
+echo "Tails: "$y "%"
